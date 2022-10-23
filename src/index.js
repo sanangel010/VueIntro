b1dbe58d8cb4712b1ed899844c73aaf3182fbc28
José Ángel Haro Juárez <sanangel010@gmail.com>
2022-10-22 23:13:59 -0500

    Se adiciona archivo de dessestructuración de arreglos

--- src/index.js
+++ src/index.js
@@ -1,54 +1,32 @@
-// DESESTRUCTURACIÓN DE OBJETOS:
-// 
+// DESESTRUCTURACIÓN DE ARREGLOS (LA POSICIÓN ES  IMPORTANTE
 
-const person = {
-    name: 'Tony',
-    age: 45,
-    codeName: 'IronMan',
-    power: 'Dinero 22'
+const characters = ['Goku','Vegeta','Trunks']
+
+const goku = characters[0]
+const vegeta = characters[1]
+const trunks = characters[2]
+
+console.log(goku, trunks)
+
+
+// desestructurando 01
+const [g,v,t] = characters
+console.log(g,v,t)
+
+
+// desestructurando 01
+const [,,tr] = characters
+console.log(tr)
+
+// desestructurando 02
+const [go,ve,tru, goten=' Sin valor'] = characters
+console.log(tru,goten)
+
+
+const returnArray = () => {
+    return ['ABC',123]
 }
 
-console.log( person.name )
-console.log( person.age )
-console.log( person.codeName )
-console.log( person.power )
-
-// Para evitar esto se realizá la desestructuración que basicamente es
-// en la función indicar las variables que se quieren ocupar el orden no es importante.
-
-const { age, name, codeName, power = 'Sin power' } = person
-
-console.log( name )
-console.log( age )
-console.log( codeName )
-console.log( power )
-
-
-// apartir de emascript 6 si los nombres de los parámetros y los valores son los mismos se puede obviar
-const createHero2 = ( {name, age, codeName, power} ) => 
-(   {
-        id: 4656,
-        name,
-        age,
-        codeName,
-        power
-    }
-)
-
-
-const createHero = ( person ) => {
-    return {
-        id: 4656111,
-        name: person.name,
-        age: person.age,
-        codeName: person.codeName,
-        power: person.power
-    }
-}
-
-console.log('heroe2: ');
-console.log( createHero2 ( person ) )
-console.log('heroe: ');
-console.log( createHero ( person ) )
-
-
+// desestructurando
+const [letters,numbers] = returnArray()
+console.log(letters,numbers)
