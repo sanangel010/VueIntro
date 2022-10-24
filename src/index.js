1dbc7767c6ff5105013e0aee1350e581aaf0afec
José Ángel Haro Juárez <sanangel010@gmail.com>
2022-10-24 12:07:13 -0500

    Se adiciona archivo para introducción a las promesas.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
-// Importaciones y exportaciones.
-
-// Exportación clasica.
-// import { owners } from './data/heroes'
-// console.log( owners )
-
-//  // Se desectructura el objetro
-// const [dc, marvel] = owners
-
-// console.log( dc )
-// console.log( marvel )
+// PROMESAS
+// Es una tarea asincrona, que permite ejecutar un código en 
+// en un diferente instante de tiempo.
+// Las promesas no necesitan importarse son parte de javascript
 
 
+// console.log('Inicio')
 
-// Exportación por defecto
-import superEdues from  './data/heroes'
+// new Promise( (resolve, reject) => {
 
-console.log(superEdues)
+//     console.log('Cuerpo de la promesa')
+    
+//     //resolve('Promesa resuelta.')
+//     reject('Promesa resuelta con error.')
 
-// Busqueda por id/int
-const getHeroeById = ( id ) => 
-    superEdues.find( (Edue) => Edue.id === id ) 
+// } )
+// // .then( msg => console.log( msg ) ) // El código del then se ejecuta hasta que el procedimiento o el hilo principal se ha terminado. 
+// // .catch( err => console.log( err ) ) // Se hace el manejo del error de la promesa
 
-console.log(getHeroeById(2))
+// // Tambien se puede hacer el llamado de la siguiente manera
+// .then( console.log ) // El código del then se ejecuta hasta que el procedimiento o el hilo principal se ha terminado. 
+// .catch( console.log ) // Se hace el manejo del error de la promesa
+
+// console.log('Fin')
 
 
-// busqueda por owner/text
-const getHeroeByOwner = ( owner ) => 
-    superEdues.filter( (Edue) => Edue.owner === owner ) 
 
-console.log(getHeroeByOwner('DC'))
+// Promesa con parámetros
+import { getHeroeById } from  './bases/08-import-export'
+
+const getHeroeByIdAsync = ( id ) => {
+    return new Promise ( ( resolve, reject) => {
+        setTimeout( () => {
+            const hero = getHeroeById( id )
+            
+            // Se hace la evaluación
+            if(hero){
+                resolve( hero )
+            }else{
+                reject( 'Heroe no existe' )
+            }            
+
+        }, 1000);
+    });
+}
+
+// Se manda a llamar.
+getHeroeByIdAsync(2)
+    .then( her => console.log(`El héroe es: ${her.name}`) )
+    .catch( console.log )
